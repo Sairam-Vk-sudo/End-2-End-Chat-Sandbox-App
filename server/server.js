942253a5259686a59de100ca6dabcf6e206cbfde
AtharvaPatil86 <atharvapatil619@gmail.com>
2026-03-23 01:46:23 +0530

    Final commit before deploying

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-
+const PORT = process.env.PORT || 5000;
 const Message = require("./models/Message");
 const authRoutes = require("./routes/auth");
 
@@ -11,20 +11,35 @@ app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
 
+    // 🔹 Get user info from client
+    const username = socket.handshake.query.username;
+    const role = socket.handshake.query.role;
+
+    console.log(`${username} connected as ${role}`);
+
     socket.on("sendMessage", async (data) => {
         const msg = new Message(data);
         await msg.save();
 
+        // 🔴 Insecure broadcast (root cause of MITM)
         io.emit("receiveMessage", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`${username} disconnected`);
     });
 
 });
 
-server.listen(5000, () => {
-    console.log("Server running on port 5000");
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
